49b40700b48e5bde8a08b7d98bf168565ad5c1fd
Simon Graf <simon.graf@statistik.ji.zh.ch>
2021-11-16 12:39:59 +0100

    loaded packages

---
+++ packages.R
@@ -0,0 +1,28 @@
+# define required packages
+requiredPackages <- c(
+  "DBI",
+  "here",
+  "janitor",
+  "lubridate",
+  "odbc",
+  "renv",
+  "ROracle",
+  "tidyverse"
+)
+
+
+# install/load required packages:
+if (exists("requiredPackages")) {
+  # install required packages that are not installed yet:
+  new.packages <- requiredPackages[!(requiredPackages %in% installed.packages()[, "Package"])]
+  if(length(new.packages)) {
+    install.packages(new.packages)
+  }
+  # load required packages:
+  lapply(requiredPackages, library, character.only = TRUE)
+}
+rm(new.packages, requiredPackages)
+
+
+# # connect db
+# source("~/db_connect.R")
