6f7df5be6216a5d808e9e9f9b6f7dc40491e35d5
Simon Graf <simon.graf@statistik.ji.zh.ch>
2021-12-19 17:24:50 +0100

    minor changes

--- hosp_death_vaccinationstatus.R
+++ hosp_death_vaccinationstatus.R
@@ -13,7 +13,7 @@ hosp_vaccpersons_age_raw1 <- hosp_vaccpersons_age_raw %>%
   # group_by(altersklasse_covid19, kw) %>%
   # mutate(comp = max(inz_entries)/min(inz_entries)) %>%
   filter(
-    kw >= 30, # use only data after week 29
+    # kw >= 30, # use only data after week 29
     altersklasse_covid19 != "Unbekannt",
     altersklasse_covid19 != "all",
     vaccination_status %in% c("fully_vaccinated", "not_vaccinated")
@@ -105,47 +105,49 @@ hosp_vaccpersons_age_allweeks <- hosp_vaccpersons_age_allweeks_raw %>%
 
 
 ## Create plots ----
-ggplot(
-  data = hosp_vaccpersons_age,
-  mapping = aes(
-    x = kw,
-    y = inz_entries,
-    group = vaccination_status,
-    colour = vaccination_status)) +
-  labs(
-    title = "Hospitalisierungsinzidenz nach Alter und Impfstatus",
-    y = "Hospitalisierungen/100'000",
-    x = "Kalenderwoche Jahr 2021",
-    colour = "Impfstatus",
-    linetype = "Impfstatus") +
-  # geom_point() +
-  geom_line(aes(
-    colour = vaccination_status,
-    linetype = vaccination_status
-    )) +
-  # geom_line(
-  #   data = hosp_vaccpersons_age[!is.na(hosp_vaccpersons_age$inz_entries_rel),],
-  #   mapping = aes(
-  #     x = kw,
-  #     y = inz_entries_rel
-  #     )
-  #   ) +
-  facet_wrap(altersklasse_covid19 ~ ., scales = "free") +
-  # scale_color_brewer(palette = "Dark2") +
-  # scale_color_discrete() +
-  scale_colour_manual(values = c(
-    "not_vaccinated" = "#B01657",
-    "fully_vaccinated" = "#00797B"
-    )) +
-  # theme(legend.position = c(0.65, 0.14)) +
-  theme_minimal()
+hosp_vaccpersons_age %>%
+  filter(kw >= 30) %>%
+  ggplot(
+    # data = hosp_vaccpersons_age,
+    mapping = aes(
+      x = kw,
+      y = inz_entries,
+      group = vaccination_status,
+      colour = vaccination_status)) +
+    labs(
+      title = "Hospitalisierungsinzidenz nach Alter und Impfstatus",
+      y = "Hospitalisierungen/100'000",
+      x = "Kalenderwoche Jahr 2021",
+      colour = "Impfstatus",
+      linetype = "Impfstatus") +
+    # geom_point() +
+    geom_line(aes(
+      colour = vaccination_status,
+      linetype = vaccination_status
+      )) +
+    # geom_line(
+    #   data = hosp_vaccpersons_age[!is.na(hosp_vaccpersons_age$inz_entries_rel),],
+    #   mapping = aes(
+    #     x = kw,
+    #     y = inz_entries_rel
+    #     )
+    #   ) +
+    facet_wrap(altersklasse_covid19 ~ ., scales = "free") +
+    # scale_color_brewer(palette = "Dark2") +
+    # scale_color_discrete() +
+    scale_colour_manual(values = c(
+      "not_vaccinated" = "#B01657",
+      "fully_vaccinated" = "#00797B"
+      )) +
+    # theme(legend.position = c(0.65, 0.14)) +
+    theme_minimal()
 
 
 # Plot COVID related deaths, grouped by vaccination status AND age ----
 
 
 ## Load and wrangle data ----
-death_vaccpersons_age_raw <- read_csv("https://www.covid19.admin.ch/api/data/20211115-ew84xikb/sources/COVID19Death_vaccpersons_AKL10_w.csv")
+death_vaccpersons_age_raw <- read_csv("https://www.covid19.admin.ch/api/data/20211217-drih76wp/sources/COVID19Death_vaccpersons_AKL10_w.csv")
 death_vaccpersons_age <- death_vaccpersons_age_raw %>%
   mutate(kw = as.numeric(str_trunc(date, 2, side = "left", ellipsis = ""))) %>%
   relocate(kw, .after = date) %>%
